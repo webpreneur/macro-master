a9bf8e68dc6364f61590483ffca972e322b5db53
Gulyás Zsolt <zsolt.gulyas@outlook.com>
2023-07-28 23:21:09 +0200

    chore: specify deno lib versions

--- server-deno/server.ts
+++ server-deno/server.ts
@@ -1,5 +1,5 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { Application } from "https://deno.land/x/oak@v12.6.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import router from "./routes.ts";
 
 const port = 8280;
