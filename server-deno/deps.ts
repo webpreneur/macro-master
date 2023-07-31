2c58b79e410e97ae34a2f0c5280fb71a5cfa1dca
Gulyás Zsolt <zsolt.gulyas@moresimp.com>
2023-07-31 09:29:06 +0200

    chore: add parse to deps

--- server-deno/deps.ts
+++ server-deno/deps.ts
@@ -4,4 +4,5 @@ export { load } from "https://deno.land/std@0.196.0/dotenv/mod.ts";
 export { Application, Router } from "https://deno.land/x/oak@v12.6.0/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 export { PoolClient, Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+export { parse } from "https://deno.land/std@0.196.0/flags/mod.ts";
 
